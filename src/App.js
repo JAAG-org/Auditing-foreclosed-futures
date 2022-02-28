4a98216ca49c42852bd061f4b184da86cbc2ff16
dodadoa <maddodadoa@gmail.com>
2022-02-28 17:24:10 +0700

    add crisis chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,70 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import Modal from 'react-modal'
+import * as d3 from 'd3'
+import './App.css'
+import csvData from './data/th_us.csv'
+
+const Chart = (props) => {
+  const d3Container = useRef(null)
+
+  useEffect(
+    () => {
+      const margin = { top: 10, right: 30, bottom: 30, left: 60 }
+      const width = 400 - margin.left - margin.right
+      const height = 200 - margin.top - margin.bottom
+
+      if (props.data && d3Container.current) {
+        const svg = d3.select(d3Container.current)
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+        d3.csv(csvData, (d) => {
+            const format = { date : d3.timeParse("%Y-%m")(d.date), value : parseFloat(d.value) }
+            return format
+        }).then(
+          (data) => {
+            const x = d3.scaleTime()
+              .domain(d3.extent(data, (d) => {
+                return d.date 
+              }))
+              .range([ 0, width ])
+            svg.append("g")
+              .attr("transform", "translate(0," + height + ")")
+              .call(d3.axisBottom(x))
+
+            const y = d3.scaleLinear()
+              .domain([0, d3.max(data, function(d) { return +d.value; })])
+              .range([ height, 0 ])
+            svg.append("g")
+              .call(d3.axisLeft(y))
+
+            svg.append("path")
+              .datum(data)
+              .attr("fill", "none")
+              .attr("stroke", "red")
+              .attr("stroke-width", 1.5)
+              .attr("d", d3.line()
+                .x(function(d) { return x(d.date) })
+                .y(function(d) { return y(d.value) })
+              )
+          }
+        )
+      }
+    }, [props.data, d3Container.current]
+  )
+
+  return (
+    <svg
+      className="d3-component"
+      width={400}
+      height={200}
+      ref={d3Container}
+    />
+  )
+}
 
 const App = () => {
   const [isModalOpen, setModalOpen] = useState(false)
@@ -15,6 +79,11 @@ const App = () => {
       >
         <h1> About </h1>
       </Modal>
+
+      <div class="chart-container">
+        <Chart data={[1, 2, 3]}/>
+      </div>
+
 
       <h1 className="header"> Auditing foreclosed futures+ </h1>
       <h3 className="subheader"> Open Reception: 20 March 2022, 4PM++ </h3>
